d8d0f233aadf161a0b02fe80078234faebdf4f55
cperiya <157152157+cperiya@users.noreply.github.com>
2024-02-16 16:30:17 -0500

    attributes and constructors for hospital, patient and Vitalsigns

--- src/models/VitalSign.java
+++ src/models/VitalSign.java
@@ -14,4 +14,55 @@ public class VitalSign {
     private String bloodPressure;
     private Double temprature;
     private Integer respiratoryRate;
+
+    public Integer getVitalsignID() {
+        return vitalsignID;
+    }
+
+    public void setVitalsignID(Integer vitalsignID) {
+        this.vitalsignID = vitalsignID;
+    }
+
+    public Integer getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(Integer heartRate) {
+        this.heartRate = heartRate;
+    }
+
+    public String getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(String bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public Double getTemprature() {
+        return temprature;
+    }
+
+    public void setTemprature(Double temprature) {
+        this.temprature = temprature;
+    }
+
+    public Integer getRespiratoryRate() {
+        return respiratoryRate;
+    }
+
+    public void setRespiratoryRate(Integer respiratoryRate) {
+        this.respiratoryRate = respiratoryRate;
+    }
+
+    public VitalSign(Integer vitalsignID, Integer heartRate, String bloodPressure, Double temprature, Integer respiratoryRate) {
+        this.vitalsignID = vitalsignID;
+        this.heartRate = heartRate;
+        this.bloodPressure = bloodPressure;
+        this.temprature = temprature;
+        this.respiratoryRate = respiratoryRate;
+    }
+
+    public VitalSign() {
+    }
 }
