17959b847d0af060a54ef3e0c97f8816d773fd42
vrindashinde <vrindashinde00@gmail.com>
2024-02-16 16:08:13 -0500

    Constructors, Getters and Setters (Admin to Encounter))

--- src/models/Community.java
+++ src/models/Community.java
@@ -11,5 +11,36 @@ package models;
 public class Community {
     private Integer communityID;
     private String name;
+
+    public Community() {
+    }
+
+    public Community(Integer communityID, String name) {
+        this.communityID = communityID;
+        this.name = name;
+    }
+
+    public Integer getCommunityID() {
+        return communityID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCommunityID(Integer communityID) {
+        this.communityID = communityID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+    
     
 }
